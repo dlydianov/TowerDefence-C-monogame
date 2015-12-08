1e883c696a3ca05d23105cc82343611669076ecf
kiselia <asen_kiselov@abv.bg>
2015-12-08 14:36:34 +0200

    enemy

---
+++ Game1/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1
+{
+    public class Enemy :Sprite
+    {
+        protected float startHealth;
+        protected float currentHealth;
+
+        protected bool alive = true;
+
+        protected float speed = 0.5f;
+        protected int bountyGiven;
+
+
+        public Enemy(Texture2D tex, Vector2 pos,float health , int bountyGiven, float speed) 
+            : base(tex, pos)
+        {
+            this.startHealth = health;
+            this.currentHealth = startHealth;
+            this.bountyGiven = bountyGiven;
+            this.speed = speed;
+        }
+
+        public float CurrentHealth
+        {
+            get { return currentHealth; }
+            set { currentHealth = value; }
+        }
+
+        public bool IsDead
+        {
+            get { return currentHealth <= 0; }
+        }
+
+        public int BountyGiven
+        {
+            get { return bountyGiven; }
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if (currentHealth <= 0)
+                alive = false;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (alive)
+            {
+                float healthPercentage = (float)currentHealth / (float)startHealth;
+
+                Color color = new Color(new Vector3(1 - healthPercentage,
+                    1 - healthPercentage, 1 - healthPercentage));
+
+                base.Draw(spriteBatch, color);
+            }
+        }
+
+
+    }
+}
