8886b7cc9dbe721b3079cc058e6c6c257113a210
kiselia <asen_kiselov@abv.bg>
2015-12-12 16:13:48 +0200

    fix

---
+++ Game1/Bullet/Bullet.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1.Bullet
+{
+    public class Bullet : Sprite
+    {
+        private int damage;
+        private int age;
+
+        private int speed;
+
+        public int Damage
+        {
+            get { return damage; }
+        }
+
+        public bool IsDead()
+        {
+            return age > 100;
+        }
+
+        public Bullet(Texture2D texture, Vector2 position, float rotation, int speed, int damage)
+            : base(texture, position)
+        {
+            this.rotation = rotation;
+            this.damage = damage;
+
+            this.speed = speed;
+
+            velocity = Vector2.Transform(new Vector2(0, -speed),
+                Matrix.CreateRotationZ(rotation));
+        }
+
+        public void Kill()
+        {
+            this.age = 200;
+        }
+
+        public void SetRotation(float value)
+        {
+            rotation = value;
+
+            velocity = Vector2.Transform(new Vector2(0, -speed),
+                Matrix.CreateRotationZ(rotation));
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            age++;
+            position += velocity;
+
+            base.Update(gameTime);
+        }
+    }
+}
